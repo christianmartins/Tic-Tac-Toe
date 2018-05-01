851e47ec0c22a3b4b302a6ffcbb5f7566653328e
Christian <crmon93@gmail.com>
2018-05-01 02:29:28 -0300

    Feat: fix insert X and O

--- js/hash.js
+++ js/hash.js
@@ -1,52 +1,14 @@
 var grid = new Array(9);
+
+var position = ["#1","#2","#3","#4","#5","#6","#7","#8","#9"];
 var numeroDeRodadas = 9;
 var rodadaAtual = 1;
-var jogadorAtual = rodadaAtual % 2 == 1? "X" : "O";
 
 $(document).ready(function(){
-	$("#1").click(function(){
-		jogadorAtual = rodadaAtual % 2 == 1? "X" : "O";
-		$("#1").html(jogadorAtual);
-		rodadaAtual++;
-	});
-	$("#2").click(function(){
-		jogadorAtual = rodadaAtual % 2 == 1? "X" : "O";
-		$("#2").html(jogadorAtual);
-		rodadaAtual++;
-	});
-	$("#3").click(function(){
-		jogadorAtual = rodadaAtual % 2 == 1? "X" : "O";
-		$("#3").html(jogadorAtual);
-		rodadaAtual++;
-	});
-	$("#4").click(function(){
-		jogadorAtual = rodadaAtual % 2 == 1? "X" : "O";
-		$("#4").html(jogadorAtual);
-		rodadaAtual++;
-	});
-	$("#5").click(function(){
-		jogadorAtual = rodadaAtual % 2 == 1? "X" : "O";
-		$("#5").html(jogadorAtual);
-		rodadaAtual++;
-	});
-	$("#6").click(function(){
-		jogadorAtual = rodadaAtual % 2 == 1? "X" : "O";
-		$("#6").html(jogadorAtual);
-		rodadaAtual++;
-	});
-	$("#7").click(function(){
-		jogadorAtual = rodadaAtual % 2 == 1? "X" : "O";
-		$("#7").html(jogadorAtual);
-		rodadaAtual++;
-	});
-	$("#8").click(function(){
-		jogadorAtual = rodadaAtual % 2 == 1? "X" : "O";
-		$("#8").html(jogadorAtual);
-		rodadaAtual++;
-	});
-	$("#9").click(function(){
-		jogadorAtual = rodadaAtual % 2 == 1? "X" : "O";
-		$("#9").html(jogadorAtual);
+
+	$(".grid").click(function(){
+		var jogadorAtual = rodadaAtual % 2 == 1? "X" : "O";
+		$(this).html(jogadorAtual);
 		rodadaAtual++;
 	});
 });
